e86c7bffb5f0100490cb26c9268545bde2221c26
daulet2030 <daulet2030@gmail.com>
2021-02-09 07:44:57 +0600

    task2 solution

---
+++ src/main/java/task2/ListOfListsReader.java
@@ -0,0 +1,33 @@
+package task2;
+
+import org.apache.poi.ss.usermodel.*;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListOfListsReader {
+    public static void main(String[] args) throws IOException {
+        Workbook workbook = WorkbookFactory.create(new File("src/main/resources/listOfLists.xlsx"));
+        Sheet sheet = workbook.getSheetAt(0);
+
+        List<List<String>> listOfLists = new ArrayList<>();
+        int numberOfRows = sheet.getPhysicalNumberOfRows();
+
+        for (int i = 0; i < numberOfRows; i++) {
+            List<String> list = new ArrayList<>();
+
+            Row row = sheet.getRow(i);
+            int numberOfCells = row.getPhysicalNumberOfCells();
+            for (int j = 0; j < numberOfCells; j++) {
+                Cell cell = row.getCell(j);
+                list.add(cell.getStringCellValue());
+            }
+
+            listOfLists.add(list);
+        }
+
+        System.out.println(listOfLists);
+    }
+}
