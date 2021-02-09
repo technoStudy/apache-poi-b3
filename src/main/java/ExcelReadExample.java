ef280a14fbbddef67c3fbfb617007d02b03ef5f6
daulet2030 <daulet2030@gmail.com>
2021-02-09 06:25:20 +0600

    reading workbook file and getting sheet

--- src/main/java/ExcelReadExample.java
+++ src/main/java/ExcelReadExample.java
@@ -1,2 +1,16 @@
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+
+import java.io.File;
+import java.io.IOException;
+
 public class ExcelReadExample {
+    public static void main(String[] args) throws IOException {
+        File file = new File("src/main/resources/Book1.xlsx");
+        Workbook workbook = WorkbookFactory.create(file);
+        Sheet sheetByIndex = workbook.getSheetAt(0);
+        Sheet sheet1 = workbook.getSheet("Sheet1");
+
+    }
 }
